e20bc58f4e267fe0aade65db25f75a1a37eb7e91
Sanjay Guwaju <sanjayguwaju@gmail.com>
2023-06-29 09:48:13 +0000

    Update 2 files

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Herosection from './components/heroSection/hero-section'
-import Ctasection from './components/heroSection/cta-section'
-import Featuresection from './components/heroSection/feature-section'
-import FormLayout from './components/form-layouts/FormLayout'
-import { Routes,Route } from 'react-router-dom'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Herosection from "./components/heroSection/hero-section";
+import Ctasection from "./components/heroSection/cta-section";
+import Featuresection from "./components/heroSection/feature-section";
+import FormLayout from "./components/form-layouts/FormLayout";
+import { Routes, Route } from "react-router-dom";
+import Logocloud from "./components/logo-clouds/LogoCloud";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <Routes>
-      <Route exact path='/' element={<>
-        <Herosection />
-        <Featuresection/>
-        <Ctasection/>
-        </>} />
-      <Route exact path='/form' element={<FormLayout/>} />
+      <Route
+        exact
+        path="/"
+        element={
+          <>
+            <Herosection />
+            <Featuresection />
+            <Ctasection />
+            <Logocloud />
+          </>
+        }
+      />
+      <Route exact path="/form" element={<FormLayout />} />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
